6832702da9f31c0e2cf3b28dfbca023cc8e5bc55
Al McKinlay <github@10people.co.uk>
2019-12-02 10:15:06 +0000

    Day 1 part 2

--- day1/fuel.js
+++ day1/fuel.js
@@ -6,6 +6,22 @@ fs.readFile("./data.txt", "utf8", (err, data) => {
 		process.exit(1);
 	}
 	const modules = data.split("\n").filter((module) => module);
-	const fuelRequired = modules.reduce((total, module) => total + (Math.floor(Number(module) / 3) - 2), 0);
+	const fuelRequired = modules.reduce((total, module) => total + fuelForModule(Number(module)), 0);
 	console.log(fuelRequired);
 });
+
+
+const fuelForModule = (module) => {
+	const fuelForMass = Math.floor(module / 3) - 2;
+	let massStillRequiringFuel = fuelForMass;
+	let fuelForFuel = 0;
+        while (massStillRequiringFuel > 0) {
+                const fuelToAdd = Math.floor(massStillRequiringFuel / 3) - 2;
+                massStillRequiringFuel = fuelToAdd;
+                if (fuelToAdd > 0) {
+                        fuelForFuel += massStillRequiringFuel;
+                }
+        }
+	return fuelForMass + fuelForFuel;
+};
+
