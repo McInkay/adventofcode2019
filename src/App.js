bfdb372b7f8ba296e67c8589233a07aa04fbeb83
Al McKinlay <github@10people.co.uk>
2019-12-04 08:56:01 +0000

    Remove basename from routing as we switched to hash routing

--- src/App.js
+++ src/App.js
@@ -9,11 +9,9 @@ import {
 
 import './App.css';
 
-const baseName = process.env.NODE_ENV === "production" ? "/adventofcode2019" : undefined; 
-
 export default function App() {
   return (
-    <Router basename={baseName}>
+    <Router>
       <div className="App">
         <header>
           <p> <Link to="/">Al's Advent of Code </Link> </p>
