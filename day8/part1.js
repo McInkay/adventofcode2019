aaed335d517f665ad795882468387c3db97aed56
Al McKinlay <github@10people.co.uk>
2019-12-09 11:13:46 +0000

    Day 8 part 1

---
+++ day8/part1.js
@@ -0,0 +1,31 @@
+module.exports = (data) => {
+	console.log(data.length);
+	const width = 25;
+	const height = 6;
+
+	const pixels = data.split("");
+	const pixelsPerLayer = width * height;
+	const numLayers = data.length / pixelsPerLayer;
+
+	const leastZeros = [151, ""];
+
+	for (let layerNum = 0; layerNum < numLayers; layerNum++) {
+		const first = layerNum * 150;
+		const last = first + 149;
+
+		const layer = data.slice(first, last + 1);
+		const zeros = layer.replace(/[^0]/g, "").length;
+		if (zeros < leastZeros[0]) {
+			leastZeros[0] = zeros;
+			leastZeros[1] = layer;
+			console.log(leastZeros);
+		}
+	}
+
+
+	const ones = leastZeros[1].replace(/[^1]/g, "").length;
+	const twos = leastZeros[1].replace(/[^2]/g, "").length;
+
+	return ones * twos;
+	
+}
